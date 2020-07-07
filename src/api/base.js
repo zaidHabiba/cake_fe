bf2bf2429ec22e18c6998ee0933ad9e406d7e037
PixelsTownTechnologies <pixelstown@hotmail.com>
2020-07-07 20:30:50 +0300

    change url

--- src/api/base.js
+++ src/api/base.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const BASE_URL = "http://localhost:8000";
-export const DEPLOY_URL = "https://cakebe.herokuapp.com";
+export const DEPLOY_URL = "https://cakebe2.herokuapp.com";
 export default axios.create({
     baseURL: DEPLOY_URL
 });
